c28f743ccfe51f6a3fe9d71f6bbd092e6bb25431
HamsterStudent <hwanyamyu@gmail.com>
2023-07-17 13:29:32 +0900

    5.2 play button use setState

--- lib/features/videos/widgets/video_post.dart
+++ lib/features/videos/widgets/video_post.dart
@@ -1,21 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:video_player/video_player.dart';
+import 'package:visibility_detector/visibility_detector.dart';
 
 class VideoPost extends StatefulWidget {
   final Function onVideoFinished;
 
+  final int index;
+
   const VideoPost({
     super.key,
     required this.onVideoFinished,
+    required this.index,
   });
 
   @override
   State<VideoPost> createState() => _VideoPostState();
 }
 
-class _VideoPostState extends State<VideoPost> {
+class _VideoPostState extends State<VideoPost>
+    with SingleTickerProviderStateMixin {
   final VideoPlayerController _videoPlayerController =
       VideoPlayerController.asset("assets/videos/video.mp4");
+  final Duration _animationDuration = const Duration(milliseconds: 200);
+  late final AnimationController _animatinoController;
+
+  bool _isPaused = false;
 
   void _onVideoChange() {
     if (_videoPlayerController.value.isInitialized) {
@@ -30,7 +41,7 @@ class _VideoPostState extends State<VideoPost> {
 
   void _initVideoPlayer() async {
     await _videoPlayerController.initialize();
-    _videoPlayerController.play();
+    // _videoPlayerController.play();
     setState(() {});
     _videoPlayerController.addListener(_onVideoChange);
   }
@@ -39,6 +50,27 @@ class _VideoPostState extends State<VideoPost> {
   void initState() {
     super.initState();
     _initVideoPlayer();
+
+    _animatinoController = AnimationController(
+      vsync: this,
+      // 시작점
+      lowerBound: 1.0,
+      // 끝나는 점
+      upperBound: 1.5,
+      // 기본값
+      value: 1.5,
+      // 시간
+      duration: _animationDuration,
+    );
+
+    // 이벤트 감지
+    _animatinoController.addListener(
+      // value의 시작점과 끝점 사이의 값들을 호출해서 build해서
+      // 애니메이션을 부드럽게 만듬
+      () {
+        setState(() {});
+      },
+    );
   }
 
   @override
@@ -47,18 +79,67 @@ class _VideoPostState extends State<VideoPost> {
     super.dispose();
   }
 
+  void _onVisibilityChanged(VisibilityInfo info) {
+    if (info.visibleFraction == 1 && !_videoPlayerController.value.isPlaying) {
+      _videoPlayerController.play();
+    }
+  }
+
+  void _onTogglePause() {
+    if (_videoPlayerController.value.isPlaying) {
+      _videoPlayerController.pause();
+      // reverse : upper -> lower
+      _animatinoController.reverse();
+    } else {
+      _videoPlayerController.play();
+      // forward : lower -> upper
+      _animatinoController.forward();
+    }
+    setState(() {
+      _isPaused = !_isPaused;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Positioned.fill(
-          child: _videoPlayerController.value.isInitialized
-              ? VideoPlayer(_videoPlayerController)
-              : Container(
-                  color: Colors.black,
+    return VisibilityDetector(
+      key: Key("${widget.index}"),
+      onVisibilityChanged: _onVisibilityChanged,
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: _videoPlayerController.value.isInitialized
+                ? VideoPlayer(_videoPlayerController)
+                : Container(
+                    color: Colors.black,
+                  ),
+          ),
+          Positioned.fill(
+            child: GestureDetector(
+              onTap: _onTogglePause,
+            ),
+          ),
+          Positioned.fill(
+              // 아이콘 클릭 이벤트 무시
+              child: IgnorePointer(
+            child: Center(
+              child: Transform.scale(
+                // 변경된 값이 들어옴
+                scale: _animatinoController.value,
+                child: AnimatedOpacity(
+                  opacity: _isPaused ? 1 : 0,
+                  duration: _animationDuration,
+                  child: const FaIcon(
+                    FontAwesomeIcons.play,
+                    color: Colors.white,
+                    size: Sizes.size52,
+                  ),
                 ),
-        )
-      ],
+              ),
+            ),
+          ))
+        ],
+      ),
     );
   }
 }
